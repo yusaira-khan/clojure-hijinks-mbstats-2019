c498f9ea7c73c4b9dc6fc138b99106b7aff61b35
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-10-14 23:27:30 -0400

    start fiddling with tenzing

---
+++ mbstats-ten/src/cljs/mbstats_ten/app.cljs
@@ -0,0 +1,7 @@
+(ns mbstats-ten.app)
+;(:require
+; [clojure.tools.nrepl])
+(defn init []
+  (let [c (.. js/document (createElement "DIV"))]
+    (aset c "innerHTML" "<p>i'm dynamically created</p>")
+    (.. js/document (getElementById "container") (appendChild c))))
