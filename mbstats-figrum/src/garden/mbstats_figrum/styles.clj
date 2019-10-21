242c2ebfab31d8955eab1960c57a7ff8db7189c5
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-10-20 22:15:37 -0400

    attempt garden selectors

--- mbstats-figrum/src/garden/mbstats_figrum/styles.clj
+++ mbstats-figrum/src/garden/mbstats_figrum/styles.clj
@@ -1,10 +1,20 @@
 (ns mbstats-figrum.styles
   (:require [garden.def :refer [defrule defstyles]]
-            [garden.stylesheet :refer [rule]]))
-
+            [garden.stylesheet :refer [rule]]
+            [garden.selectors :refer [defclass selector]]
+            ))
+(defclass bold)
 (defstyles screen
-  (let [body (rule :body)]
+  (let [body (rule :body)
+        bold (rule "bold")
+        ]
     (body
      {:font-family "Helvetica Neue"
       :font-size   "16px"
-      :line-height 1.5})))
+      :line-height 1.5})
+    (bold {
+           :font-weight "bold";
+           :font-size "24px"
+           })
+    ))
+
