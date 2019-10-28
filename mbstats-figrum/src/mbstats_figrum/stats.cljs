6545221d5dfb0c65ad9f5a2ef41541e7d925e230
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-10-28 02:16:00 -0400

    get all titles

--- mbstats-figrum/src/mbstats_figrum/stats.cljs
+++ mbstats-figrum/src/mbstats_figrum/stats.cljs
@@ -1,6 +1,11 @@
 (ns mbstats-figrum.stats)
 
 
-(def title {:intro "Introvert",
-            :extro "Extrovert",
-            :judge "Judging"} )
+(def title {:intro "Introvert"
+            :extro "Extrovert"
+            :judge "Judging"
+            :perce "Perceiving"
+            :sense "Sensing"
+            :intui "Intuition"
+            :think "Thinking"
+            :feel "Feeling"} )
